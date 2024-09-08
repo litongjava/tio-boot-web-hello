17cd790427de94b92155a5694b3f123ecf82edb1
litongjava <litongjava@qq.com>
2024-09-08 00:48:21 -1000

    update to v1.5.2

--- src/main/java/com/litongjava/tio/web/hello/config/WebHelloConfig.java
+++ src/main/java/com/litongjava/tio/web/hello/config/WebHelloConfig.java
@@ -2,14 +2,18 @@ package com.litongjava.tio.web.hello.config;
 
 import java.io.IOException;
 
+import com.litongjava.jfinal.aop.annotation.AConfiguration;
+import com.litongjava.jfinal.aop.annotation.AInitialization;
 import com.litongjava.tio.boot.context.TioBootConfiguration;
 import com.litongjava.tio.boot.server.TioBootServer;
 import com.litongjava.tio.http.server.router.RequestRoute;
 import com.litongjava.tio.web.hello.handler.HelloHandler;
 
+@AConfiguration
 public class WebHelloConfig implements TioBootConfiguration {
 
   @Override
+  @AInitialization
   public void config() throws IOException {
     HelloHandler helloHandler = new HelloHandler();
 
---
+++ src/main/java/com/litongjava/tio/web/hello/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.litongjava.tio.web.hello.controller;
+
+import com.jfinal.kit.Kv;
+import com.litongjava.tio.http.server.annotation.RequestPath;
+import com.litongjava.tio.utils.resp.RespVo;
+
+@RequestPath("/base")
+public class BaseController {
+
+  public RespVo date(java.util.Date date) {
+    Kv kv = Kv.create();
+    kv.set("date", date);
+    return RespVo.ok(kv);
+  }
+}
